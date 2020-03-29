1b4434870a608bd9d313f6abc11c7503a1bfefee
Albert Lacasta <47864341+AlbertLacasta@users.noreply.github.com>
2020-03-30 01:10:07 +0200

    created the home for the gui

--- gui/src/views/Home.vue
+++ gui/src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <v-card tile @click="goToMenu">
+        <v-img
+                class="white--text align-end"
+                :src="images.background"
+        >
+            <v-card-title class="display-3 ml-3">{{timestamp}}</v-card-title>
+            <v-card-text class="mt-0 ml-3">
+                <v-row align="center" class="pt-0 mt-0">
+                    <v-col class="display-1 pt-0 mt-0" cols="6" >
+                        23&deg;C
+                    </v-col>
+                </v-row>
+            </v-card-text>
+        </v-img>
+    </v-card>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'Home',
+        components: {
+        },
+        data() {
+            return {
+                images: {
+                    background: require('@/images/gradient.jpg')
+                },
+                timestamp: ""
+            }
+        },
+
+        created() {
+            setInterval(this.getNow, 1000);
+        },
+        methods: {
+            getNow() {
+                const today = new Date();
+                const time = today.getHours() + ":" + today.getMinutes();
+                this.timestamp = time;
+            },
+
+            goToMenu() {
+                this.$router.push('Menu')
+            }
+        }
+    }
+
 </script>
---
+++ gui/src/views/Menu.vue
@@ -0,0 +1,13 @@
+<template>
+    <p>this is the menu</p>
+</template>
+
+<script>
+    export default {
+        name: "Menu"
+    }
+</script>
+
+<style scoped>
+
+</style>
