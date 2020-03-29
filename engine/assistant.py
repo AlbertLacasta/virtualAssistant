04d8b1de8cb3a9a498b97de31f51dc0b58886401
Albert Lacasta <47864341+AlbertLacasta@users.noreply.github.com>
2020-03-29 23:21:43 +0200

    added ElectronJS

--- engine/assistant.py
+++ engine/assistant.py
@@ -29,18 +29,20 @@ def assistant(command):
 		reg_ex = re.search("application (.*)", command)
 		application = reg_ex.group(1)
 		aplication.open(application)
-	elif 'hello' in command:
+	elif "hello" in command:
 		date.welcome()
-	elif 'help me' in command:
+	elif "help me" in command:
 		help()
-	elif 'make a joke' in command:
+	elif "make a joke" in command:
 		joke.makeJoke()
-	elif 'tell me about' in command:
+	elif "tell me about" in command:
 		reg_ex = re.search('tell me about (.*)', command)
 		chrome.about(reg_ex)
-	elif 'shut down' in command:
+	elif "shut down" in command:
 		speak('Bye bye Sir. Have a nice day')
 		sys.exit()					
-	elif 'what is the weather in' in command:
+	elif "what is the weather in" in command:
 		reg_ex = re.search('what is the weather in(.*)', command)
 		weather.tellWeather(reg_ex)
+	elif "i go outside" in command:
+		speak("don't be idiot and stay at home")
